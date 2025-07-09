2f7a0fc0c26d6758d2a7d788113440700fb4beae
Fourth <fourthe.hero@gmail.com>
2025-07-09 23:09:33 +0700

    feat: implement map position tooltip

--- app/map/component/mapPositionPoint/mapPositionPoint.tsx
+++ app/map/component/mapPositionPoint/mapPositionPoint.tsx
@@ -7,16 +7,15 @@ type Props = {
 
 export const MapPositionPoint =(props:Props)=> {
     return (
-        <div>
+        <div className="tooltip">
             <div 
-                className="w-8 h-8 rounded-full bg-black bg-opacity-25 cursor-pointer flex items-center justify-center border-gray-400 border-2"
+                className="w-8 h-8 rounded-full bg-black bg-opacity-25 cursor-pointer flex items-center justify-center border-gray-400 border-2 hover:bg-opacity-55"
                 data-tooltip-target="tooltip-default">
                 <div className="map-position-point-inside"></div>
             </div>
-            <div id="tooltip-default" role="tooltip" className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-xs opacity-0 tooltip dark:bg-gray-700">
-    Tooltip content
-    <div className="tooltip-arrow" data-popper-arrow></div>
-</div>
+            <div className="tooltiptext">               
+                <h1>{props.location_name}</h1>
+            </div>
         </div>
 
     )
--- app/map/spring_school/components/mapPosition/mapPosition.tsx
+++ app/map/spring_school/components/mapPosition/mapPosition.tsx
@@ -5,16 +5,16 @@ export default function MapPosition(){
     return (
         <div className="map-position-container">
             <div className='map-position-building-rooftop'>
-                <MapPositionPoint link='building-rooftop' location_name='building-rooftop' location_image='' />
+                <MapPositionPoint link='building-rooftop' location_name='building rooftop' location_image='' />
             </div>
             <div className='map-position-club-room'>
-                <MapPositionPoint link='club-room' location_name='club-room' location_image=''/>
+                <MapPositionPoint link='club-room' location_name='club room' location_image=''/>
             </div>
             <div className='map-position-library'>
                 <MapPositionPoint link='library' location_name='library' location_image=''/>
             </div>
             <div className='map-position-student-council'>
-                <MapPositionPoint link='student-council' location_name='student-council' location_image=''/>
+                <MapPositionPoint link='student-council' location_name='student council' location_image=''/>
             </div>
         </div>
     )
